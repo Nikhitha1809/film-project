64aa7d2f9d51d236a94f1384592651b68f8bcc55
Nikhitha Chilukuri (iTalent Corporation) <chilukuri.nikhitha1999@gmail.com>
2024-09-24 14:34:34 +0530

    code change

--- src/Wishlist/Wishlist.jsx
+++ src/Wishlist/Wishlist.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import './wishList.css';
+import './WishList.css';
 import { WishlistContext } from '../WishlistContext/WishlistContext';
 const Wishlist = () => {
   const location = useLocation();
